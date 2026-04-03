ab155f0af553a49df1ab6d7e141f477598c31f38
Gil Dhason <agil_dhason@infotechy.com>
2026-04-03 13:59:48 -0400

    Added function to save donor, but it must be checked

--- Data/Donor.py
+++ Data/Donor.py
@@ -1,9 +1,22 @@
+import json
+
 class Donor:
-    def __init__(self, donorID, name, address):
-        self.donorID = donorID
+    def __init__(self, donor_id, name, address):
+        self.donor_id = donor_id
         self.name = name
         self.address = address
-        self.items = []
+        self.item_ids = [] # list of item IDs (strings)
 
-    def addItem(self, item):
+    # TODO: Implement
+    def add_item(self, item):
         self.items.append(item)
+
+    def save_donor(self):
+        data = {
+            "donor_id": self.donor_id,
+            "name": self.name,
+            "address": self.address,
+            "items": self.item_ids
+        }
+        with open(f"{self.donor_id}.json", "w") as f:
+            f.dump(data, f)
